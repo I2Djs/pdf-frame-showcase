6595ad956772c8cdc909f969cd9e8d38d3f822be
Narayana Swamy <narayanaswamy14@gmail.com>
2024-06-01 01:22:23 +0530

    ui layout chnages

--- app.vue
+++ app.vue
@@ -30,12 +30,12 @@
       <v-main class="d-flex justify-center">
         <v-container fluid class="d-flex flex-column pl-12 pr-12 pt-5" style="height: auto;">
           <v-row class="fill-width px-10 pt-6 pb-4 justify-center d-flex">
-            <v-label class="text-h2 text-center font-weight-bold main-banner-title" justify="space-around" :stacked="true"  position="sticky" border=0 >
+            <v-label class="text-h2 text-center font-weight-bold main-banner-title" :class=" smAndDown ? 'text-h4' : 'text-h2'" justify="space-around" :stacked="true"  position="sticky" border=0 >
                 PDF-Frame
             </v-label>
           </v-row>
           <v-row class="fill-width px-10">
-            <v-banner class="justify-center d-flex text-center font-weight-medium main-banner pt-0" :class=" smAndDown ? 'text-subtitle-1' : 'text-h6'" color="purple-accent-2" justify="space-around" :stacked="true"  position="sticky" border=0 >
+            <v-banner class="justify-center d-flex text-center font-weight-medium main-banner pt-0" :class=" smAndDown ? 'text-subtitle-2' : 'text-h6'" color="purple-accent-2" justify="space-around" :stacked="true"  position="sticky" border=0 >
                 <template v-slot:text>
                   JavaScript framework for client-side PDF and Canvas rendering. Effortlessly integrate with Vue3 and Nuxt3 for dynamic graphical content creation.
                 </template>
